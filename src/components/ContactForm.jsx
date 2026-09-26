35885f7e2854eb140e703d158814a153548ef67b
Ramlakhan Lodhi <ramlakhanlodhi229@gmail.com>
2026-09-27 00:40:59 +0530

    Updated for Add logo

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -2,7 +2,7 @@ import {
     Turnstile,
 } from "@marsidev/react-turnstile";
 import { useState } from "react";
-const turnstileSiteKey = '0x4AAAAAABzsHb0oW1jrht7C';
+const turnstileSiteKey = '0x4AAAAAAFEkhMTpI7yxwsvH';
 
 const useBasinFormId = '768f9cda9ef4';
 
